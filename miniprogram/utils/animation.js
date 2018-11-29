d59bb642c21ac4f08174347484d766cad3d7f098
Zengzx <zengzx@fxiaoke.com>
2018-11-29 15:25:52 +0800

    demo2 developing

---
+++ miniprogram/utils/animation.js
@@ -0,0 +1,49 @@
+const animation = {
+  slideUp(duration = 600) {
+    const Animation = wx.createAnimation({
+      duration,
+      timingFunction: "linear",
+      delay: 0,
+      transformOrigin: "ease-out"
+    });
+    Animation.top(-getApp().globalData["screenHeight"])
+      .opacity(0.9)
+      .step();
+    return Animation.export();
+  },
+  slideDown(duration = 600) {
+    const Animation = wx.createAnimation({
+      duration,
+      timingFunction: "linear",
+      delay: 0,
+      transformOrigin: "ease-out"
+    });
+    Animation.top(getApp().globalData["screenHeight"])
+      .opacity(0.9)
+      .step();
+    return Animation.export();
+  },
+  fadeIn(duration = 400) {
+    const Animation = wx.createAnimation({
+      duration,
+      timingFunction: "linear",
+      delay: 0,
+      transformOrigin: "ease-out"
+    });
+    Animation.opacity(1).step();
+    // Animation.top(0).step();
+    return Animation.export();
+  },
+  fadeOut(duration = 400) {
+    const Animation = wx.createAnimation({
+      duration,
+      timingFunction: "linear",
+      delay: 0,
+      transformOrigin: "ease-out"
+    });
+    Animation.opacity(0).step().top(100000).step();
+    // Animation.top(1000).step();
+    return Animation.export();
+  },
+};
+module.exports = animation;
